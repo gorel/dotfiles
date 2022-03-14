ae178fd3093184b923ec2f40100f85de79cbbbbd
Logan Gore <gorel@fb.com>
2022-03-14 10:00:25 -0700

    Remove redundant keymaps

--- nvim/after/plugin/keymaps.lua
+++ nvim/after/plugin/keymaps.lua
@@ -21,26 +21,6 @@ keymap("v", ">", ">gv", default_opts)
 -- Paste over currently selected text without yanking it
 keymap("v", "p", '"_dP', default_opts)
 
--- Telescope
-keymap("n", "<leader>fo", ":lua require'telescope.builtin'.find_files()<CR>", default_opts)
-keymap("n", "<leader>fr", ":lua require'telescope.builtin'.oldfiles()<CR>", default_opts)
-keymap("n", "<leader>fg", ":lua require'telescope.builtin'.live_grep()<CR>", default_opts)
-keymap("n", "<leader>fh", ":lua require'telescope.builtin'.help_tags()<CR>", default_opts)
-keymap("n", "<leader>fb", ":lua require'telescope.builtin'.buffers()<CR>", default_opts)
-
--- Buffer management
-keymap("n", "<leader>bn", ":bnext<CR>", default_opts)
-keymap("n", "<leader>bp", ":bprevious<CR>", default_opts)
-keymap("n", "<leader>b1", "<Cmd>BufferLineGoToBuffer 1<CR>", default_opts)
-keymap("n", "<leader>b2", "<Cmd>BufferLineGoToBuffer 2<CR>", default_opts)
-keymap("n", "<leader>b3", "<Cmd>BufferLineGoToBuffer 3<CR>", default_opts)
-keymap("n", "<leader>b4", "<Cmd>BufferLineGoToBuffer 4<CR>", default_opts)
-keymap("n", "<leader>b5", "<Cmd>BufferLineGoToBuffer 5<CR>", default_opts)
-keymap("n", "<leader>b6", "<Cmd>BufferLineGoToBuffer 6<CR>", default_opts)
-keymap("n", "<leader>b7", "<Cmd>BufferLineGoToBuffer 7<CR>", default_opts)
-keymap("n", "<leader>b8", "<Cmd>BufferLineGoToBuffer 8<CR>", default_opts)
-keymap("n", "<leader>b9", "<Cmd>BufferLineGoToBuffer 9<CR>", default_opts)
-
 -- Cancel search highlighting with ESC
 keymap("n", "<ESC>", ":nohlsearch<Bar>:echo<CR>", default_opts)
 
