f2ef52cd8373a46574b275edcdcb2c4446c08063
Logan Gore <gorel@fb.com>
2022-06-08 19:59:24 -0700

    Proper filetype support

---
+++ nvim/lua/config/filetype.lua
@@ -0,0 +1,18 @@
+local M = {}
+
+function M.setup()
+  local filetype = require "filetype"
+
+  filetype.setup({
+    overrides = {
+      extensions = {
+        cinc = "python",
+        cconf = "python",
+        mcconf = "python",
+      },
+    }
+  })
+
+end
+
+return M
