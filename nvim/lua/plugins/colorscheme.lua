6508bcf29da3ab1c06675caef2e3213a25c98a33
Logan Gore <lgore93@gmail.com>
2023-05-30 11:12:25 -0700

    Try a new colorscheme

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "hardhackerlabs/theme-vim",
+    name = "hardhacker",
+    lazy = false,
+    priority = 1000,
+    init = function()
+      vim.g.hardhacker_darker = 0
+      vim.g.hardhacker_hide_tilde = 1
+      vim.g.hardhacker_keyword_italic = 1
+    end,
+    config = function()
+      vim.cmd("colorscheme hardhacker")
+    end,
+  },
+}
