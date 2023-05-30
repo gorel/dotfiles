759b5a52543af5ef377c7fb31ec7a9ed61b43751
Logan Gore <lgore93@gmail.com>
2023-05-30 11:24:18 -0700

    More colorschemes

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -12,5 +12,34 @@ return {
     config = function()
       vim.cmd("colorscheme hardhacker")
     end,
+    enabled = false,
+  },
+  {
+    "bluz71/vim-nightfly-colors",
+    name = "nightfly",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd("colorscheme nightfly")
+    end,
+    enabled = false,
+  },
+  {
+    "sainnhe/sonokai",
+    name = "sonokai",
+    lazy = false,
+    priority = 1000,
+    init = function()
+      vim.g.sonokai_style = "default"
+      -- vim.g.sonokai_style = "atlantis"
+      -- vim.g.sonokai_style = "andromeda"
+      -- vim.g.sonokai_style = "shusia"
+      -- vim.g.sonokai_style = "maia"
+      -- vim.g.sonokai_style = "espresso"
+      -- vim.g.sonokai_better_performance = 1
+    end,
+    config = function()
+      vim.cmd("colorscheme sonokai")
+    end,
   },
 }
