4d95a65ddae25da5cf8537f65188767f9311c935
Logan Gore <lgore93@gmail.com>
2023-12-30 12:22:19 -0600

    Fix refactoring bindings

--- nvim/lua/plugins/refactoring.lua
+++ nvim/lua/plugins/refactoring.lua
@@ -17,13 +17,13 @@ return {
       })
     end,
     keys = {
-      { "<leader>re", ":Refactor extract", desc = "Extract function" },
-      { "<leader>rf", ":Refactor extract_to_file", desc = "Extract function to file" },
-      { "<leader>rv", ":Refactor extract_var", desc = "Extract variable" },
-      { "<leader>rI", ":Refactor inline_func", desc = "Inline function" },
-      { "<leader>ri", ":Refactor inline_var", desc = "Inline variable" },
-      { "<leader>rb", ":Refactor extract_block", desc = "Extract block" },
-      { "<leader>rbf", ":Refactor extract_block_to_file", desc = "Extract block to file" },
+      { "<leader>re", ":Refactor extract<cr>", desc = "Extract function" },
+      { "<leader>rf", ":Refactor extract_to_file<cr>", desc = "Extract function to file" },
+      { "<leader>rv", ":Refactor extract_var<cr>", desc = "Extract variable" },
+      { "<leader>rI", ":Refactor inline_func<cr>", desc = "Inline function" },
+      { "<leader>ri", ":Refactor inline_var<cr>", desc = "Inline variable" },
+      { "<leader>rb", ":Refactor extract_block<cr>", desc = "Extract block" },
+      { "<leader>rbf", ":Refactor extract_block_to_file<cr>", desc = "Extract block to file" },
     },
   },
   {
