fbfbd501208bb14a639f9c93a8b3b973fd3125b2
Logan Gore <lgore93@gmail.com>
2025-05-14 13:37:08 -0700

    Fix neoscroll (again)

--- nvim/lua/plugins/neoscroll.lua
+++ nvim/lua/plugins/neoscroll.lua
@@ -2,6 +2,6 @@ return {
   "karb94/neoscroll.nvim",
   opts = {
     easing_function = "linear",
-    mappings = { "<C-u>", "<C-d>", "<C-j>", "<C-k>", "zz", "zt", "zb" }, -- omit 'n' and 'N'
+    mappings = { "<C-u>", "<C-d>", "zz", "zt", "zb" }, -- omit 'n' and 'N'
   },
 }
